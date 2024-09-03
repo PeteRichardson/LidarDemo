0c3b92c9134c80c92c4399a53ccdb8c4a1c78b6b
Pete Richardson <pete@peterichardson.com>
2024-09-03 09:18:40 -0700

    Initial Commit - just Hello, World swiftui

---
+++ LidarDemo/LidarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidarDemoApp.swift
+//  LidarDemo
+//
+//  Created by Peter Richardson on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LidarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
